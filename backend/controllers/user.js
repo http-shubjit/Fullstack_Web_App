8052e979b387f5f13faab7f16d5664748f80a185
Shubhajit <shubjitgithub@gmail.com>
2024-09-17 20:12:37 +0530

    Commmit

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -15,7 +15,8 @@ async function GetAllUser(req, res) {
 async function createUser(req, res) {
   try {
     const newUser = new User(req.body);
-    await newUser.save();
+     await newUser.save();
+  
     res.status(201).json({ message: "User created", user: newUser });
   } catch (err) {
     res.status(400).json({ error: "Error creating user", details: err.message });
@@ -38,13 +39,20 @@ async function getUserById(req, res) {
 
 async function updateUser(req, res) {
   try {
-    const user = await User.findOneAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const userId = parseInt(req.params.id); 
+    const user = await User.findOneAndUpdate(
+      { userId: userId }, 
+      { password: req.body.password }, 
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
+
     res.status(200).json({ message: "User updated", user });
   } catch (err) {
     res.status(400).json({ error: "Error updating user", details: err.message });

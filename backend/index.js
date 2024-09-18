e72afcaa8d481e17af47d50086415786600ae4e8
Shubhajit <shubjitgithub@gmail.com>
2024-09-18 21:07:42 +0530

    Comit

--- backend/index.js
+++ backend/index.js
@@ -33,5 +33,5 @@ app.use("/users",useRouter);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
